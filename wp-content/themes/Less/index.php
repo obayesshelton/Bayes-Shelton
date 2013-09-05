50b32f14a287ef5413bcb994b99a09de071bec8b
Oliver Bayes-shelton <oliver@Olivers-MacBook-Pro-2.local>
2013-09-05 01:45:53 +0100

    analytics

--- wp-content/themes/Less/index.php
+++ wp-content/themes/Less/index.php
@@ -12,6 +12,17 @@
 <link href="https://fonts.googleapis.com/css?family=Raleway:400,200,800" rel="stylesheet" type="text/css">
 <script src="https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js" type="text/javascript" async=""></script>
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-41726147-4', 'bayes-shelton.co.uk');
+  ga('send', 'pageview');
+
+</script>
+
 </head>
 
 <body <?php body_class(); ?>>
