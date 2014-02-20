89ccaa7800eb95415e9dc96f6986b871b12ee43e
server <server@bayes-shelton.co.uk>
2014-02-20 07:35:06 -0500

    Bens changes for blog back to home

--- wp-content/themes/bayes-shelton/content.php
+++ wp-content/themes/bayes-shelton/content.php
@@ -12,13 +12,12 @@
 
 <div class="container">
     <section id="heading">
-        <section id="#header_nav" class="header">
-
+ <section id="#header_nav" class="header">
+<span class="take-me-back"><a href="/blog"><i class="fa fa-long-arrow-left"></i>back</a></span>
             <h1><?php the_title(); ?></h1>
             <span class="posted">posted on <?php the_date(); ?></span>
 
             <span class="take-me-uptop" onclick="toTop()"><i class="fa fa-long-arrow-up"></i>top</span>
-<span class="take-me-back"><a href="/blog"><i class="fa fa-long-arrow-left"></i>back</a></span>
        </section>
     </section>
 
